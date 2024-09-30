4463bfe3b6212a3c95fa877c7fc43c51f23a5d5c
DgDevHub <cavaleirooficia@gmail.com>
2024-09-30 17:01:09 -0300

    Atualização na validação de dados com loop e prompt

--- js/home.js
+++ js/home.js
@@ -10,12 +10,16 @@ const nomeInput = document.getElementById("nomeUser")
 const startBotao = document.getElementById("start")
 
 function verificarCampo(){
-    if(nomeInput.value.trim() !== "" && nomeInput.value.length >= 3 && isNaN(nomeInput.value) ){
-        sessionStorage.setItem("nomeUser", nomeInput.value)
-        startBotao.style.visibility = "visible"
-    }else{
-        startBotao.style.visibility = "hidden"
-         alert("Digite seu nome corretamente")
-        nomeInput.value = ""
-    }
+    let nomeValido = false
+    do {
+        if(nomeInput.value.trim() !== "" && nomeInput.value.length >= 3 && isNaN(nomeInput.value) ){
+            sessionStorage.setItem("nomeUser", nomeInput.value)
+            startBotao.style.visibility = "visible"
+            nomeValido = true
+        }else{
+            startBotao.style.visibility = "hidden"
+             alert("Digite seu nome corretamente")
+             nomeInput.value = prompt("Digite seu nome corretamente (Mínimo de 3 caracteres)")
+        }
+    } while (!nomeValido);
 }
